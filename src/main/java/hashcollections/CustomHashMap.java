3c2812d93b4a6ea977aa60f0eca36dc43104a40d
Filip Skiba <filipskiba@gmail.com>
2019-08-08 20:11:51 +0200

    Creating implementing travis.

--- src/main/java/hashcollections/CustomHashMap.java
+++ src/main/java/hashcollections/CustomHashMap.java
@@ -1,13 +1,22 @@
 package hashcollections;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.lang.reflect.Field;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class CustomHashMap <K, V> implements Map {
+public class CustomHashMap <K, V> implements Map<K, V> {
 
-    private HashMap<EntryImpl, V> storage;
+    private HashSet<EntryImpl<K, V>> storage;
+
+    public CustomHashMap() {
+        this.storage = new HashSet<>();
+    }
+
+
+    @Override
+    public String toString() {
+        return  this.storage.toString();
+    }
 
     @Override
     public int size() {
@@ -30,22 +39,29 @@ public class CustomHashMap <K, V> implements Map {
     }
 
     @Override
-    public Object get(Object key) {
+    public V get(Object key) {
+        return getCurrentValue((K) key);
+    }
+
+    @Override
+    public V put(K key, V value) {
+
+        EntryImpl<K, V> entry = new EntryImpl<>(key, value);
+        if (storage.contains(entry)) {
+            storage.remove(entry);
+        }
+        storage.add(entry);
+        return value;
+
+    }
+
+    @Override
+    public V remove(Object key) {
         return null;
     }
 
     @Override
-    public Object put(Object key, Object value) {
-        return null;
-    }
-
-    @Override
-    public Object remove(Object key) {
-        return null;
-    }
-
-    @Override
-    public void putAll(Map m) {
+    public void putAll(Map<? extends K, ? extends V> m) {
 
     }
 
@@ -55,49 +71,82 @@ public class CustomHashMap <K, V> implements Map {
     }
 
     @Override
-    public Set keySet() {
-        return null;
+    public Set<K> keySet() {
+        return storage.stream()
+                .map(EntryImpl::getKey)
+                .collect(Collectors.toSet());
     }
 
     @Override
-    public Collection values() {
-        return null;
+    public Collection<V> values() {
+        return storage.stream()
+                .map(EntryImpl::getValue)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Set<Entry> entrySet() {
-        return null;
+    public Set<Entry<K, V>> entrySet() {
+        return new HashSet<>(storage);
     }
+
+    private V getCurrentValue(K key) {
+
+        //Refleksja jest tutaj niewykorzystywana
+        try {
+            Field field = storage.getClass().getDeclaredField("map");
+            field.setAccessible(true);
+            HashMap<K, V> map = (HashMap<K, V>) field.get(storage);
+            return storage.stream()
+                    .filter(entry -> entry.getKey().equals(key))
+                    .findFirst()
+                    .map(EntryImpl::getValue)
+                    .orElse(null);
+
+        } catch (NoSuchFieldException | IllegalAccessException | ClassCastException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     public static class EntryImpl<K, V> implements Entry<K, V>{
+
+        private K key;
+        private V value;
+
+        public EntryImpl(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
 
         @Override
         public K getKey() {
-            return null;
+            return key;
         }
 
         @Override
         public V getValue() {
-            return null;
+            return value;
         }
 
         @Override
         public V setValue(V value) {
-            return null;
+
+            return this.value = value;
         }
 
         @Override
         public int hashCode() {
-            return super.hashCode();
+            return key.hashCode();
         }
 
         @Override
         public boolean equals(Object obj) {
-            return super.equals(obj);
+            return Objects.equals(obj, key);
         }
 
         @Override
         public String toString() {
-            return super.toString();
+            return key + "=" + value;
         }
     }
 }
